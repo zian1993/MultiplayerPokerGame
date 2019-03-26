2a84d7f60b3c7ff70c5bbf8024bf6dc5051fc63a
Zian <Zian@Zian-PC>
2019-03-26 16:38:39 -0500

    1st commit-Done by King.

---
+++ TeamProject/client/MyClass.java
@@ -0,0 +1,18 @@
+package client;
+
+public class MyClass
+{
+
+  public static void main(String[] args)
+  {
+    //Print statements
+    System.out.println("Hello Joe");
+    
+    System.out.println("Another print statement 2");
+    
+    System.out.println("3rd Print statement");
+
+  }
+
+}
+
